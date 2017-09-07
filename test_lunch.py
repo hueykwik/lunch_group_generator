0803e8fe28b5d5108d406cd65abe3604b1e95197
Huey Kwik <huey.kwik@gmail.com>
2017-09-06 22:25:38 -0700

    Add CLI test for adding and groups.

--- test_lunch.py
+++ test_lunch.py
@@ -3,15 +3,49 @@ from click.testing import CliRunner
 
 import lunch
 
+SIX_PERSON = ['Huey', 'Dewey', 'Louie', 'Webby', 'Donald', 'Scrooge']
+
+def test_empty_roster():
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        result = runner.invoke(lunch.cli, ['groups'])
+
+        assert result.exit_code == 0
+        assert 'Roster is currently less than three people' in result.output
+
 def test_groups():
     runner = CliRunner()
 
-    roster = ['Huey', 'Dewey', 'Louie', 'Webby', 'Donald', 'Scrooge']
+    roster = SIX_PERSON
 
     with runner.isolated_filesystem():
         with open(lunch.ROSTER_FILE, 'w') as f:
             for person in roster:
                 f.write("%s\n" % person)
+
+        result = runner.invoke(lunch.cli, ['groups'])
+
+        assert result.exit_code == 0
+        assert 'Group 1' in result.output
+        assert 'Group 2' in result.output
+
+def test_add():
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        result = runner.invoke(lunch.cli, ['add'], input='huey')
+
+        assert result.exit_code == 0
+        assert 'huey' in result.output
+
+def test_add_groups():
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        roster = SIX_PERSON
+        for person in roster:
+            result = runner.invoke(lunch.cli, ['add'], input=person)
 
         result = runner.invoke(lunch.cli, ['groups'])
 
